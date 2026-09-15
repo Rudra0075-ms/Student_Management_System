592187f21ad8061a9d15e55fa066b5630654a63d
Rudra Madhab Sahoo <sahoorudramadhab2007@gmail.com>
2026-09-15 16:37:27 +0530

    Update settings.py

--- student_project/settings.py
+++ student_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost","elite-abide-stingy.ngrok-free.dev"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     "https://elite-abide-stingy.ngrok-free.dev",
 ]
